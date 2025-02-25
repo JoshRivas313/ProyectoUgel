15b2e82252ed6e1977a7901f9a4fff09ef415139
Jose <hurtadorivasj123@gmail.com>
2025-02-24 19:27:07 -0500

    NavBar Diseño Parte 02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { InicioComponent } from "./pages/inicio/inicio.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [
+    RouterOutlet,
+    NavBarComponent,
+    InicioComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
